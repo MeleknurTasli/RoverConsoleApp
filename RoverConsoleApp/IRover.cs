8c076be9e02668b4b17884a7d2a0364ca8e30918
unknown <melekselcen@gmail.com>
2022-10-08 12:31:40 +0300

    IRover interface, RoverClass added and MoveRover method added

---
+++ RoverConsoleApp/IRover.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverConsoleApp
+{
+    interface IRover
+    {
+        char getLetterOfDirection(int angle);
+        int getAngle(char letterOfDirection);
+        void MoveRover(string moveInputs, RoverPosition roverPosition, Plateau plateau);
+        void LimitsOfPlateau(string upperRightInput);
+    }
+}
