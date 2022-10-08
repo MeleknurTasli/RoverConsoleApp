3f0fa361a9913e2aa1546fed0413efd49c1f7aa6
PC <PC@LAPTOP-70R9SQ3H>
2022-10-08 11:30:11 +0300

    Add project files.

---
+++ RoverConsoleApp/Plateau.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverConsoleApp
+{
+    class Plateau
+    {
+        public int UpperCorner { get; set; }
+        public int RightCorner { get; set; }
+        public readonly int LowerCorner = 0;
+        public readonly int LeftCorner = 0;
+    }
+}
---
+++ RoverConsoleApp/RoverPosition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverConsoleApp
+{
+    class RoverPosition
+    {
+        public int Xcoordinate { get; set; }
+        public int Ycoordinate { get; set; }
+
+        private char _letterOfDirection;
+        public char LetterOfDirection 
+        { 
+            get { return _letterOfDirection; }
+            set 
+            {
+                if (value == 'N' || value == 'S' || value == 'W' || value == 'E')
+                {
+                    _letterOfDirection = value;
+                }
+                else _letterOfDirection = '-';
+            }
+        }
+        public int Angle { get; set; }
+    }
+}
