49e355cc88d53484bdc9d7f2f9f3f8aa13660d93
unknown <melekselcen@gmail.com>
2022-10-08 11:58:45 +0300

    getLetterOfDirection and getAngle fonks written

---
+++ RoverConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using RoverConsoleApp;
+
+char getLetterOfDirection(int angle)
+// get the direction according to angle with given info
+{
+    if (angle == 0)
+        return 'N';
+    else if (angle == 90)
+        return 'E';
+    else if (angle == 180)
+        return 'S';
+    else // (angle == 270)
+        return 'W';
+}
+
+int getAngle(char letterOfDirection)
+// get the angle according to direction with given info
+{
+    if (letterOfDirection == 'N')
+        return 0;
+    else if (letterOfDirection == 'E')
+        return 90;
+    else if (letterOfDirection == 'S')
+        return 180;
+    else //(letterOfDirection == 'W')
+        return 270;
+}
+
